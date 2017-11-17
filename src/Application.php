0d1aa5f428fce66fe8a8722f8ab3c024f46267a7
Vladimir <backend@div26.ru>
2017-11-17 18:46:50 +0300

    подключил symfony/console и настроил базовое приложение

---
+++ src/Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vladimir
+ * Date: 16.11.17
+ */
+
+namespace div\DeployAgent;
+
+use Symfony\Component\Console\Application as ConsoleApplication;
+
+class Application extends ConsoleApplication
+{
+
+}
