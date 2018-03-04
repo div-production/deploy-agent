bceb068f58dc6b2cd7ff37d54dfb4a82d9a144fa
Vladimir <backend@div26.ru>
2018-03-04 13:56:11 +0300

    Сделал отдельный файл инициализации

---
+++ app.php
@@ -0,0 +1,22 @@
+<?php
+
+use div\DeployAgent\Application;
+use div\DeployAgent\ClearOpCacheCommand;
+use div\DeployAgent\InitCommand;
+use div\DeployAgent\SelfUpdateCommand;
+use div\DeployAgent\ShowLastOutputCommand;
+use div\DeployAgent\StartCommand;
+use div\DeployAgent\StartWebDeployCommand;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$__app__ = new Application('Инструмент для развётрывания php приложений', '1.2.2');
+
+$__app__->add(new InitCommand());
+$__app__->add(new StartCommand());
+$__app__->add(new StartWebDeployCommand());
+$__app__->add(new ClearOpCacheCommand());
+$__app__->add(new SelfUpdateCommand());
+$__app__->add(new ShowLastOutputCommand());
+
+return $__app__;
